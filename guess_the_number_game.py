91cfee908182ed512df6378df15d7abbc17bf155
KrzysztofCalus <krzysztofcalus@gmail.com>
2021-02-13 09:59:07 +0100

    game in funcion and docstring

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -2,19 +2,21 @@ import random
 
 pc_number = random.randint(1, 101)
 
-while True:
-    user_number = input("Guess the number:")
-    try:
-        user_number = int(user_number)
-    #    print(pc_number)
-        if user_number == pc_number:
-            print("You win!")
-            break
-        elif user_number > pc_number:
-            print("To big")
-        else:
-            print("To small")
-    except ValueError:
-        print("It's not a number!")
+def game():
+    """Allows the user to guess a randomly selected number by computer in range 1-100"""
+    while True:
+        user_number = input("Guess the number:")
+        try:
+            user_number = int(user_number)
+            if user_number == pc_number:
+                print("You win!")
+                break
+            elif user_number > pc_number:
+                print("To big")
+            else:
+                print("To small")
+        except ValueError:
+            print("It's not a number!")
 
+game()
 
